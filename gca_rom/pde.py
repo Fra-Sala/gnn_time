5f1bc33e5af64e19424f7928f120f51645ccf479
Francesco Sala <francesco.sala@epfl.ch>
2023-10-09 18:31:51 +0200

    ready to run a simulation on a gpu for the case of Fourier expansion (4 coefficients), on a dataset of 16 simulation, with time 0<t<2

--- gca_rom/pde.py
+++ gca_rom/pde.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import ipdb
 
 
 def problem(argument):
@@ -99,32 +99,43 @@ def problem(argument):
         case 11:
             problem_name = "lid_cavity"
             variable = 'U'
-            #with open('alphaM.npy', 'rb') as f:
-    
-#             mu1 = np.array([[-1.56164241, 1.69642353, -0.77954267, -2.50894837, 1.17784446],
-#                 [-8.81802628, -5.47931022, -5.43882871, -1.52585026, -2.36874186]])
-
-#             mu1 = mu1.flatten()  # fourier coefficents (right now 5 for each simulation)
-#             mu2 = np.linspace(0.0, 2., 10) #time
-#             mu_space = [mu1, mu2]
-#             n_param = 6
-        
-            # mu_range = [(-1.56164241, -8.81802628), (1.69642353, -5.47931022), (-0.77954267, -5.43882871), (-2.50894837, -1.52585026), (1.17784446, -2.36874186), (0.0, 1.8)]
-            # mu_space = []
-            # n_pts = [2]*(len(mu_range)-1)+[10]
-            # for i in range(len(mu_range)):
-            #     mu_space.append(np.linspace(mu_range[i][0], mu_range[i][1], n_pts[i]))
-            # n_param = 6
+            
+            """ Fourier expansion approach (4 frequencies)  """
             
             # Load the matrix from alphaM.npy
             alphaM = np.load('../lid_driven_cavity_fenics/alphaM.npy')
-            alphaM_cols = [alphaM[:, i].tolist() for i in range(alphaM.shape[1])]
-            mu1 = alphaM_cols[0]
-            mu2 = alphaM_cols[1]
-            mu3 = alphaM_cols[2]
-            mu4 = alphaM_cols[3]
-            mu5 = np.linspace(0.0,1.8,10)
-            mu_space = [mu1, mu2, mu3, mu4,mu5]
-            n_param = 5
+            K = alphaM.shape[0]
+            n_coeff = alphaM.shape[1]
+            mu_space = []
+            for i in range(n_coeff):
+                mu_space.append(alphaM[:, i].tolist())
+            mu_time = np.linspace(0.0, 1.8, 10)
+            mu_space.append(mu_time.tolist())
+            n_param = n_coeff + 1
+            ipdb.set_trace()
+            
+#             alphaM_cols = [alphaM[:, i].tolist() for i in range(alphaM.shape[1])]
+            
+#             mu1 = alphaM_cols[0]
+#             mu2 = alphaM_cols[1]
+#             mu3 = alphaM_cols[2]
+#             mu4 = alphaM_cols[3]
+#             mu5 = np.linspace(0.0,1.8,10)
+#             mu_space = [mu1, mu2, mu3, mu4,mu5]
+#             n_param = 5
+            
+            """ Non causal (?) approach  """
+            
+#             u_t_matrix = np.load('../lid_driven_cavity_fenics/u_t_matrix.npy')
+#             u_t_matrix_rows = [u_t_matrix[i, :].tolist() for i in range(u_t_matrix.shape[0])]
+#             mu_space = []
+#             for i in range(u_t_matrix.shape[0]):
+#                 mu_space.append(u_t_matrix_rows[i])
+#             mu_space.append(np.linspace(0.0, 1.8, 10))
+#             ipdb.set_trace()      
+#             n_param = 2
+            
+            
+            
 
     return problem_name, variable, mu_space, n_param
